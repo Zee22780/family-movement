bd672c7d529766577f1f3654881a9a41c74cecfa
zuri227 <zuri22780@gmail.com>
2026-01-08 19:33:00 -0500

    Initial Commit

---
+++ Family Movement App/Family_Movement_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Family_Movement_AppApp.swift
+//  Family Movement App
+//
+//  Created by Zuri Lyons on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Family_Movement_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
